13bf82395c98e5d65c27928f3843fa66acfa168b
Daniele Senko <daniele.senko@unemat.br>
2026-01-23 19:17:42 -0400

    chore: initial project setup with vite, ts and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-3xl font-bold text-blue-500 underline">
+        JusCleaner Funcionando!
+      </h1>
+    </div>
+  )
+}
