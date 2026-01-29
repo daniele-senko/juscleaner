9367c6f2815936f33c0c58e7536667b4be318fc3
Daniele Senko <daniele.senko@unemat.br>
2026-01-28 20:42:27 -0400

    fix(backend): install ilovepdf dependency and add type declarations

---
+++ backend/src/types.d.ts
@@ -0,0 +1,9 @@
+declare module '@ilovepdf/ilovepdf-nodejs' {
+  const ILovePDFApi: any;
+  export default ILovePDFApi;
+}
+
+declare module '@ilovepdf/ilovepdf-nodejs/ILovePDFFile' {
+  const ILovePDFFile: any;
+  export default ILovePDFFile;
+}
